0474d7e2439c6f63d593ec6861b63cbc5650fc29
Turalyusubov <tu201906152@code.edu.az>
2023-02-02 22:22:47 +0400

    created pic-some-app

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div>
+            <footer>
+                <p>Made by <a href='https://github.com/Turalyusubov'>Tural Yusubov</a></p>
+            </footer>
+        </div>
+
+    )
+}
